f3ceddb6ed802a49a459a9eeaf18fb091e913a66
Grayson Equinox <duubz@skid.pro>
2014-12-06 02:03:38 -0600

    Update urls.py

--- urls.py
+++ urls.py
@@ -9,5 +9,10 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^$',f.Pages().Index,name='Index'),
-    url(r'^user/(?P<username>\w+)/$',f.Pages().Profile,name='Profile')
+    url(r'^user/(?P<username>\w+)/$',f.Pages().Profile,name='Profile'),
+    url(r'^member/login/$',f.Pages().Login,name='Login'),
+    url(r'^member/login/doLogin/$',f.Pages().doLogin,name='doLogin'),
+    url(r'^member/register/$',f.Pages().Register,name='Register'),
+    url(r'^member/register/doRegister/$',f.Pages().doRegister,name='doRegister'),
+    url(r'^member/logout/$',f.Pages().doLogout,name='doLogout')
 )
