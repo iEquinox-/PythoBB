bc66d395cf6bdd5114a45d3ab74e38ba885b04e6
Grayson Equinox <duubz@skid.pro>
2014-12-06 00:02:23 -0600

    Update function.js

--- templates/js/function.js
+++ templates/js/function.js
@@ -1,26 +1,27 @@
 jQuery.noConflict();
 
 // https://docs.djangoproject.com/en/dev/ref/csrf/
-
-function getCookie(name){
-	var cookieValue = null;
-	if (document.cookie && document.cookie != ''){
-		var cookies = document.cookie.split(';');
-		for (var i = 0; i < cookies.length; i++) {
-			var cookie = jQuery.trim(cookies[i]);
-			if (cookie.substring(0,name.length+1) == (name+'=')) {
-				cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-				break;
+function doCSRF() {
+	function getCookie(name){
+		var cookieValue = null;
+		if (document.cookie && document.cookie != ''){
+			var cookies = document.cookie.split(';');
+			for (var i = 0; i < cookies.length; i++) {
+				var cookie = jQuery.trim(cookies[i]);
+				if (cookie.substring(0,name.length+1) == (name+'=')) {
+					cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+					break;
+				}
 			}
 		}
+		return cookieValue;
 	}
-	return cookieValue;
+
+	var csrftoken = getCookie('csrftoken');
+
+	// Change token
+
+	jQuery(document).ready(function($){
+		$("input.CSRFToken").attr('value',csrftoken);
+	});
 }
-
-var csrftoken = getCookie('csrftoken');
-
-// Change token
-
-jQuery(document).ready(function($){
-	$("input.CSRFToken").attr('value',csrftoken);
-});
