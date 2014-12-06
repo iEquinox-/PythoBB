a1378dbe331ab2eb5745fd2d6058fd42e135c4f5
Grayson Equinox <duubz@skid.pro>
2014-12-05 23:22:05 -0600

    Create getCSRF.js

---
+++ templates/js/getCSRF.js
@@ -0,0 +1,26 @@
+jQuery.noConflict();
+
+// https://docs.djangoproject.com/en/dev/ref/csrf/
+
+function getCookie(name){
+	var cookieValue = null;
+	if (document.cookie && document.cookie != ''){
+		var cookies = document.cookie.split(';');
+		for (var i = 0; i < cookies.length; i++) {
+			var cookie = jQuery.trim(cookies[i]);
+			if (cookie.substring(0,name.length+1) == (name+'=')) {
+				cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+				break;
+			}
+		}
+	}
+	return cookieValue;
+}
+
+var csrftoken = getCookie('csrftoken');
+
+// Change token
+
+jQuery(document).ready(function($){
+	$("input.CSRFToken").attr('value',csrftoken);
+});
