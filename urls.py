6746fe63699c2dca6593375bdaf0fdf5d43bd8d5
Grayson Equinox <duubz@skid.pro>
2014-12-07 02:26:34 -0600

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,23 +1,28 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-import os
-import InstallPage as i
 import frame as f
 
-#installed = open(os.getcwd()+"/pythobb/install","r").read()
-
-#if installed.replace("\n","") == "False":
-#	xPage = url(r'^$',i.PageOut().Init,name='Init')
-#else:
-xPage = url(r'^$',f.Pages().Index,name='Index')
+try:
+	installed = open("/home/equinox/pythobb/pythobb/install","r").read()
+	if installed.replace("\n","") == "False":
+		import InstallPage as i
+		indexPage = url(r'^$',i.PageOut().Init,name='Init')
+		urlpatterns + ( url(r'^admin/doConfigure/',i.PageOut().doConfigure,name='doConfigure') )
+	else:
+		indexPage = url(r'^$',f.Pages().Index,name='Index')
+except:
+	xPage = url(r'^$',i.PageOut().Init,name='Init')
+	urlpatterns + ( url(r'^admin/doConfigure/',i.PageOut().doConfigure,name='doConfigure') )
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pythobb.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     # url(r'^admin/', include(admin.site.urls)),
-    xPage,
+    indexPage,
     url(r'^user/(?P<username>\w+)/$',f.Pages().Profile,name='Profile'),
+    url(r'^member/controlpanel/$',f.Pages().userCP,name='userCP'),
+    url(r'^member/modifySettings/$',f.Pages().modifySettings,name='modifySettings'),
     url(r'^member/login/$',f.Pages().Login,name='Login'),
     url(r'^member/login/doLogin/$',f.Pages().doLogin,name='doLogin'),
     url(r'^member/register/$',f.Pages().Register,name='Register'),
