b8cf9fc2156e14b5b383c860e530c5e8eb728dcf
Grayson Equinox <duubz@skid.pro>
2014-12-08 03:59:02 -0600

    Create banned.py

---
+++ templates/banned.py
@@ -0,0 +1,7 @@
+	{[userblock]}
+	<div id='container'>
+		<div id='forums' style='text-align:center;'>
+			<span style='color:#FFF;font-family:tohama,droid sans,sans-serif;'>You have been banned from these forums.</span>
+		</div>
+	</div>
+	<div id='bottomblock'></div>
